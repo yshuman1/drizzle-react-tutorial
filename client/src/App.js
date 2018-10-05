ae6603b2461ae4a3a0e8141c363ec0c94229bc7c
Yasin Shuman <email@yasin.io>
2018-10-05 10:55:23 -0700

    have an error, go through tutorial and find what you missed when you had wifi issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ReadString from './ReadString'
 
 class App extends Component {
+  state = { loading: true, drizzleState: null }
+
+  componentDidMount() {
+    const { drizzle } = this.props
+
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState()
+
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState })
+      }
+    })
+  }
+
+  compomentWillUnmount() {
+    this.unsubscribe()
+  }
+
   render() {
+    if (this.state.loading) return 'Loading Drizzle...'
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ReadString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
